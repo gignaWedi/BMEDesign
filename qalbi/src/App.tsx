67e4ed23e43a601a03082cdcddf67c6ca9a057fe
gignaWedi <52723027+gignaWedi@users.noreply.github.com>
2023-08-03 19:29:40 -0500

    Data Hook Implemented, App Routing in progress.

--- qalbi/src/App.tsx
+++ qalbi/src/App.tsx
@@ -7,13 +7,16 @@ import {
   IonTabBar,
   IonTabButton,
   IonTabs,
-  setupIonicReact
+  setupIonicReact,
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { heartCircleOutline, square, triangle } from 'ionicons/icons';
 import GraphTab from './pages/GraphTab';
 import AdviceTab from './pages/AdviceTab';
 import SettingsTab from './pages/SettingsTab';
+import { useState, useEffect } from 'react';
+import { fetchRecords } from './hooks/DataHook';
+import { Filesystem, Encoding, Directory } from '@capacitor/filesystem';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -36,41 +39,122 @@ import './theme/variables.css';
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <GraphTab />
-          </Route>
-          <Route exact path="/tab2">
-            <AdviceTab />
-          </Route>
-          <Route path="/tab3">
-            <SettingsTab />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon aria-hidden="true" icon={heartCircleOutline} />
-            <IonLabel>HRV</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  
+  const userSettings = {
+    upperHRVState: useState<number>(),
+    lowerHRVState: useState<number>(),
+    passcodeState: useState<number>()
+  }
+
+  const userState = useState<number>();
+
+  var readjustError = 0;
+  
+  const hrvCallback = async (rawRecord:DataView): Promise<void> => {
+    await storeRecord(rawRecord);
+    await determineUserState();
+  };
+  
+  const storeRecord = async (rawRecord:DataView): Promise<void> => {
+    const timestamp = rawRecord.getInt32(0, true);
+    const rmssd = rawRecord.getFloat32(4, true).toFixed(2).padStart(6);
+
+    const record = `${timestamp} ${rmssd}\n`;
+
+    const currentDatetime = new Date(timestamp);
+        
+    const year = currentDatetime.getUTCFullYear().toString().padStart(4, '0');
+    const month = currentDatetime.getUTCMonth().toString().padStart(2, '0');
+    const day = currentDatetime.getUTCDay().toString().padStart(2, '0');
+
+    const filename = `HRV-${year}${month}${day}.txt`;
+
+    try {
+      const contents = await Filesystem.readFile({
+        path: filename,
+        directory: Directory.Data,
+        encoding: Encoding.UTF8
+      });
+      
+      if (!contents.data.includes(record)){
+        await Filesystem.appendFile({
+          path: filename,
+          data: record,
+          directory: Directory.Data,
+          encoding: Encoding.UTF8
+        });
+      }
+
+    } catch (error) {
+      await Filesystem.writeFile({
+        path: filename,
+        data: record,
+        directory: Directory.Data,
+        encoding: Encoding.UTF8
+      });
+    }
+
+  }
+  
+  const determineUserState = async (): Promise<void> => {
+    const baselineRecords = await fetchRecords(3 * 24 * 60 * 60);
+    const sampleRecords = await fetchRecords(3 * 60 * 60);
+
+    if (baselineRecords.length <= 0 || sampleRecords.length <= 0){
+      return;
+    }
+
+    const baselineHRV = baselineRecords.map((record) => record[1]).reduce((acc, curr) => acc + curr, 0) / baselineRecords.length;
+    const sampleHRV = sampleRecords.map((record) => record[1]).reduce((acc, curr) => acc + curr, 0) / baselineRecords.length;
+
+    const upperHRV = userSettings.upperHRVState[0];
+    const lowerHRV = userSettings.lowerHRVState[0];
+
+    if (sampleHRV > 107 || sampleHRV > 1.15 * baselineHRV || upperHRV != undefined && sampleHRV > upperHRV)
+      userState[1](1);
+    else if (sampleHRV < 16 || sampleHRV < 0.85 * baselineHRV || lowerHRV != undefined && sampleHRV < lowerHRV)
+      userState[1](-1);
+    else
+      userState[1](0);
+  }
+
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonTabs>
+          <IonRouterOutlet>
+            <Route exact path="/tab1">
+              <GraphTab />
+            </Route>
+            <Route exact path="/tab2">
+              <AdviceTab />
+            </Route>
+            <Route path="/tab3">
+              <SettingsTab />
+            </Route>
+            <Route exact path="/">
+              <Redirect to="/tab1" />
+            </Route>
+          </IonRouterOutlet>
+          <IonTabBar slot="bottom">
+            <IonTabButton tab="tab1" href="/tab1">
+              <IonIcon aria-hidden="true" icon={triangle} />
+              <IonLabel>Tab 1</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="tab2" href="/tab2">
+              <IonIcon aria-hidden="true" icon={heartCircleOutline} />
+              <IonLabel>HRV</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="tab3" href="/tab3">
+              <IonIcon aria-hidden="true" icon={square} />
+              <IonLabel>Tab 3</IonLabel>
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
+      </IonReactRouter>
+    </IonApp>
+  );
+};
 
 export default App;
---
+++ qalbi/src/hooks/DataHook.tsx
@@ -0,0 +1,138 @@
+import { BleClient, numberToUUID } from "@capacitor-community/bluetooth-le";
+import { Preferences } from "@capacitor/preferences";
+import { Directory, Filesystem, Encoding } from "@capacitor/filesystem";
+
+const DEVICE_ID = "device_id" // Preference for Device Wearable ID.
+
+// BLE Service and Characteristic UUIDs
+export const HRV_SERVICE = numberToUUID(0x180F);
+export const HRV_CHARACTERISTIC = numberToUUID(0x2A19);
+export const ERROR_CHARACTERISTIC = numberToUUID(0x2A1A);
+export const REQUEST_CHARACTERISTIC = numberToUUID(0x2A1B);
+
+await BleClient.initialize(); // Start the BleClient
+
+export const dataHook = async (callbacks:Array<(value:DataView) => void>) => {
+    try {
+        var connected = false;
+        var id: string;
+
+        do {
+            const {value} = await Preferences.get({key: DEVICE_ID});
+
+            if (value) {
+                id = value;
+            }
+            else {
+                const device = await BleClient.requestDevice({
+                    services: [HRV_SERVICE]
+                })
+                
+                id = device.deviceId;
+                await Preferences.set( {key: DEVICE_ID, value: id});
+            }
+            
+            var attempts = 0;
+            while (!connected && attempts < 5) {
+                try {
+                    await BleClient.connect(id, () => onDisconnect());
+                    connected = true;
+
+                } catch (error) {
+                    attempts++;
+                }
+            }
+
+            if (!connected) {
+                await Preferences.remove({key: DEVICE_ID});
+            }
+
+        } while (!connected);
+
+        await BleClient.startNotifications(
+            id,
+            HRV_SERVICE,
+            HRV_CHARACTERISTIC,
+            callbacks[0]
+        );
+
+        await BleClient.startNotifications(
+            id,
+            HRV_SERVICE,
+            ERROR_CHARACTERISTIC,
+            callbacks[1]
+        );
+
+        const lastWeekDate = new Date(Date.now() - 6.048e+8);
+
+        // Create an ArrayBuffer
+        const buffer = new ArrayBuffer(8);
+        
+        // Create a view
+        const view = new DataView(buffer);
+        
+        // Write Request
+        view.setUint16(0, lastWeekDate.getUTCFullYear(), true);
+        view.setUint8(2, lastWeekDate.getUTCMonth());
+        view.setUint8(3, lastWeekDate.getUTCDate());
+
+        await BleClient.write(
+            id,
+            HRV_SERVICE,
+            REQUEST_CHARACTERISTIC,
+            view
+        )
+
+
+    } catch (error) {
+        console.error(error);
+    }
+
+    function onDisconnect(): void {
+        dataHook(callbacks);
+    }
+}
+
+export const fetchRecords = async(timePeriod:number): Promise<number[][]> => {
+    var records: number[][] = [];
+
+    var rawData = "";
+
+    var endingTimestamp = Date.now();
+
+    var startTimestamp = endingTimestamp - 1000*timePeriod;
+
+    var currentTimestamp = startTimestamp;
+    
+    while (currentTimestamp < endingTimestamp) {
+        const currentDatetime = new Date(currentTimestamp);
+        
+        const year = currentDatetime.getUTCFullYear().toString().padStart(4, '0');
+        const month = currentDatetime.getUTCMonth().toString().padStart(2, '0');
+        const day = currentDatetime.getUTCDay().toString().padStart(2, '0');
+
+        const filename = `HRV-${year}${month}${day}.txt`;
+        
+        try {
+            const contents = await Filesystem.readFile({
+                path: filename,
+                directory: Directory.Data,
+                encoding: Encoding.UTF8,
+            });
+            rawData += contents.data
+        } catch (error) {
+            console.error(`${filename} doesn't exist`)
+        }
+
+        currentTimestamp += 8.64e+7;
+    }
+
+    if (rawData != ""){
+        const allRecords = rawData.split("\n").map(element => element.split(' ').map(e => Number(e)));
+        records = allRecords.filter((row) => row[0] >= startTimestamp && row[0] <= endingTimestamp);
+
+        records.sort((record1, record2) => record1[0] - record2[1]);
+    }
+
+    return records;
+}
