8122bc63cf3508cea0bdc5494f959c84367dd073
gignaWedi <52723027+gignaWedi@users.noreply.github.com>
2023-08-03 21:08:43 -0500

    Graph tab shold start with no data

--- qalbi/src/pages/GraphTab.tsx
+++ qalbi/src/pages/GraphTab.tsx
@@ -53,17 +53,11 @@ export const options = {
   }
 };
 
-
 const GraphTab: React.FC<{userSettings:Settings}> = ({userSettings}) => {
   
   const [chartData, setChartData] = useState<any>({
-    labels: [...Array(7).keys()],
+    labels: [],
     datasets: [{
-      label: 'My First Dataset',
-      data: [65, 59, 80, 81, 56, 55, 40],
-      fill: false,
-      borderColor: 'rgb(75, 192, 192)',
-      tension: 0.1
     }]
   });
 
